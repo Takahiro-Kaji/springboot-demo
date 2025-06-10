bb7d0f61b74fd82d8906f553c733c610f857edfd
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2025-06-11 01:07:09 +0900

    Ldaps

--- src/main/java/com/example/demo/service/ActiveDirectoryService.java
+++ src/main/java/com/example/demo/service/ActiveDirectoryService.java
@@ -12,10 +12,12 @@ public class ActiveDirectoryService {
     protected DirContext connect() throws NamingException {
         Hashtable<String, String> env = new Hashtable<>();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.PROVIDER_URL, "ldap://" + System.getenv("AD_HOST") + ":389");
+        env.put(Context.PROVIDER_URL, "ldaps://" + System.getenv("AD_HOST") + ":636");
         env.put(Context.SECURITY_AUTHENTICATION, "simple");
         env.put(Context.SECURITY_PRINCIPAL, "CN=Administrator,CN=Users,DC=sandbox,DC=local");
         env.put(Context.SECURITY_CREDENTIALS, System.getenv("AD_PASSWORD"));
+
+        env.put("java.naming.ldap.factory.socket", "javax.net.ssl.SSLSocketFactory");
         return new InitialDirContext(env);
     }
 }
